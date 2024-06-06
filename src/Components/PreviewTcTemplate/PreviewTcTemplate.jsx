aaf6b9f5cfb5fa803e7bb43914a6c21659ebe6fb
Krishnanand A <krishnanand654@gmail.com>
2024-06-06 18:29:44 +0530

    production debug

--- src/Components/PreviewTcTemplate/PreviewTcTemplate.jsx
+++ src/Components/PreviewTcTemplate/PreviewTcTemplate.jsx
@@ -1,6 +1,6 @@
 import { usePDF } from 'react-to-pdf';
 import { Button } from '@nextui-org/react';
-import './previewtcTemplate.css';
+// import './previewtcTemplate.css';
 import { useSelector, useDispatch } from 'react-redux';
 
 
