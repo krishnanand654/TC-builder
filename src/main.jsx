93a61fea0a9586395438426d6eba0e04587224cb
Krishnanand A <krishnanand654@gmail.com>
2024-06-10 02:01:39 +0530

    routing debug

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,14 @@ import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
+
+
+    <BrowserRouter>
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </Provider>
-    </NextUIProvider>
+    </BrowserRouter>
+
+
   </React.StrictMode>,
 )
